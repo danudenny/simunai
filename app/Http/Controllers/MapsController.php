4f4e6b032756f5c421951d1c56bf3de0ce1e5e59
LembayungProject <denny.danuwijaya@gmail.com>
2022-10-03 09:18:59 +0000

    change group by

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -33,7 +33,7 @@ class MapsController extends Controller
                 )
             ) as feature_layer"))
             ->leftJoin('kecamatan', 'kecamatan.id', 'jalan.kecamatan_id')
-            ->groupBy('jalan.id', 'kecamatan.nama')
+            ->groupBy('jalan.id', 'kecamatan.nama', 'jalan.nama_ruas', 'jalan.kondisi_jalan','jalan.status_jalan','jalan.panjang','jalan.lebar','jalan.jenis_perkerasan','jalan.kelas_jalan','jalan.geojson','jalan.style','jalan.kecamatan_id','jalan.created_at','jalan.updated_at','jalan.th_data','jalan.mendukung','jalan.uraian_dukungan','jalan.titik_pengenal_awal','jalan.titik_pengenal_akhir','jalan.kode_patok','jalan.baik','jalan.sedang','jalan.rusak_ringan','jalan.rusak_berat','jalan.mantap','jalan.tidak_mantap','jalan.geom')
             ->get();
         $kecamatan = Kecamatan::all();
         return view('pages.maps.index', compact('data', 'kecamatan'));
